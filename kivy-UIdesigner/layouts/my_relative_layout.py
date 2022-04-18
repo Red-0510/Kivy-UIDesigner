ce7b6701e61ec375d0e22c784108fc88890f355a
Red-0510 <69681031+Red-0510@users.noreply.github.com>
2022-04-18 14:58:05 +0530

    Delete kivy-UIdesigner directory

--- kivy-UIdesigner/layouts/my_relative_layout.py
+++
@@ -1,7 +0,0 @@
-from kivy.uix.relativelayout import RelativeLayout
-class My_Relative_Layout(RelativeLayout):
-    def __init__(self,pos,parent_object,keys,**kwargs):
-        poshints={'x':pos[0]/parent_object.width,'y':pos[1]/parent_object.height}
-        size=(pos[2]/parent_object.width,pos[3]/parent_object.height)
-        self.heirarchy={}
-        super(My_Relative_Layout,self).__init__(pos_hint=poshints,size_hint=size,**keys,**kwargs)

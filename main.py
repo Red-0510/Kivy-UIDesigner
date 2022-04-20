996962dc7a5ece25916e911342a2a81e09a55910
Red-0510 <69681031+Red-0510@users.noreply.github.com>
2022-04-20 15:25:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -23,7 +23,24 @@ class DrawingSpace(RelativeLayout):
         self.type=None
         self.args=None
         self.heirarchy={'root':self}
-        self.layout={}#{"id":{parent,type,arguments,pos_hints,size_hints}}
+        self.layout={'Header': {'parent': '', 'type': 'RL', 'arguments': {}, 'size_hint': [0.985, 0.2275], 'pos_hint': {'x': 0.00625, 'y': 0.753}},
+                    'Title': {'parent': 'Header', 'type': 'LL', 'arguments': {'markup': 'True', 'font_size': '50sp', 'text': '[b]The Sasti Dukan[/b]'}, 'size_hint': [0.6230964467005076, 0.3142857142857143], 'pos_hint': {'x': 0.19035532994923857, 'y': 0.6476190476190476}},
+                    'Name': {'parent': 'Header', 'type': 'LL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Name:[/b]'}, 'size_hint': [0.18781725888324874, 0.26666666666666666], 'pos_hint': {'x': 0.006345177664974619, 'y': 0.3333333333333333}}, 
+                    'Date': {'parent': 'Header', 'type': 'LL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Date:[/b]'}, 'size_hint': [0.18781725888324874, 0.22857142857142856], 'pos_hint': {'x': 0.006345177664974619, 'y': 0.06666666666666667}}, 
+                    'Name_input': {'parent': 'Header', 'type': 'TI', 'arguments': {}, 'size_hint': [0.7017766497461929, 0.26666666666666666], 'pos_hint': {'x': 0.19923857868020303, 'y': 0.3142857142857143}}, 
+                    'Date_input': {'parent': 'Header', 'type': 'TI', 'arguments': {}, 'size_hint': [0.26903553299492383, 0.23809523809523755], 'pos_hint': {'x': 0.19923857868020303, 'y': 0.03809523809523864}}, 
+                    'Time': {'parent': 'Header', 'type': 'LL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Time:[/b]'}, 'size_hint': [0.18274111675126903, 0.24761904761904707], 'pos_hint': {'x': 0.4987309644670051, 'y': 0.028571428571429112}}, 
+                    'Time_input': {'parent': 'Header', 'type': 'TI', 'arguments': {}, 'size_hint': [0.20939086294416243, 0.2571428571428566], 'pos_hint': {'x': 0.6916243654822335, 'y': 0.028571428571429112}}, 
+                    'Item_details': {'parent': '', 'type': 'RL', 'arguments': {}, 'size_hint': [0.98625, 0.6174999999999999], 'pos_hint': {'x': 0.00625, 'y': 0.12466666666666675}}, 
+                    'Functional_buttons': {'parent': '', 'type': 'RL', 'arguments': {}, 'size_hint': [0.9825, 0.10616666666666666], 'pos_hint': {'x': 0.0075, 'y': 0.009833333333333414}}, 
+                    'Item_name': {'parent': 'Item_details', 'type': 'LL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Item Name[/b]'}, 'size_hint': [0.46134347275031684, 0.11228070175438616], 'pos_hint': {'x': 0.0063371356147021544, 'y': 0.8736842105263155}}, 
+                    'Quantity': {'parent': 'Item_details', 'type': 'LL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Quantity[/b]'}, 'size_hint': [0.1761723700887199, 0.10877192982456141], 'pos_hint': {'x': 0.4740177439797212, 'y': 0.8736842105263155}}, 
+                    'Rate': {'parent': 'Item_details', 'type': 'LL', 'arguments': {'markup': 'True', 'font_size': '20sp', 'text': '[b]Rate(1 piece)[/b]'}, 'size_hint': [0.17110266159695817, 0.10877192982456141], 'pos_hint': {'x': 0.6552598225602028, 'y': 0.8736842105263155}}, 
+                    'Total': {'parent': 'Item_details', 'type': 'LL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Total[/b]'}, 'size_hint': [0.16096324461343473, 0.11578947368421072], 'pos_hint': {'x': 0.8326996197718631, 'y': 0.8701754385964909}}, 
+                    'Add_item': {'parent': 'Functional_buttons', 'type': 'BL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Add Item[/b]'}, 'size_hint': [0.3053435114503817, 0.8367346938775504], 'pos_hint': {'x': 0.005089058524173028, 'y': 0.08163265306122391}}, 
+                    'Total_up': {'parent': 'Functional_buttons', 'type': 'BL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Total Up[/b]'}, 'size_hint': [0.356234096692112, 0.8163265306122444], 'pos_hint': {'x': 0.3155216284987278, 'y': 0.10204081632653003}},
+                    'Print': {'parent': 'Functional_buttons', 'type': 'BL', 'arguments': {'markup': 'True', 'font_size': '25sp', 'text': '[b]Print[/b]'}, 'size_hint': [0.32061068702290074, 0.7959183673469382], 'pos_hint': {'x': 0.6755725190839694, 'y': 0.12244897959183615}},
+                    }#{"id":{parent,type,arguments,pos_hints,size_hints}}
         super(DrawingSpace,self).__init__(**kwargs)
     def on_touch_down(self, touch):
         if self.parent.add_button.button.state=='down' and self.collide_point(touch.x,touch.y):
@@ -90,6 +107,8 @@ class DrawingSpace(RelativeLayout):
             print(k,v)
     def go_to_App_Screen(self,button):
         # print('Generating Layout')
+        print(self.heirarchy)
+        print(self.layout)
         self.parent.parent.parent.current='app_screen'
         self.parent.parent.parent.app_screen.generate_layout(self.layout)
     def delete_widget(self,button):
@@ -98,6 +117,25 @@ class DrawingSpace(RelativeLayout):
             widget[1].parent.remove_widget(widget[1])
             self.layout.pop(widget[0])
             self.parent.parent.parent.app_screen.delete_widget(widget[0])
+    def initialize_layout(self):
+        self.parent.parent.parent.app_screen.generate_layout(self.layout)
+        for k,v in self.layout.items():
+            if k in self.heirarchy:
+                continue
+            if v['parent']=='' or v['parent']=='root':
+                parent_object=self
+            else:
+                parent_object=self.heirarchy[v['parent']]
+            if v['type']=='RL':
+                widget=my_relative_layout.My_Relative_Layout(v['pos_hint'],v['size_hint'],v['arguments'])
+            elif v['type']=='BL':
+                widget=my_button.My_Button_Layout(v['pos_hint'],v['size_hint'],v['arguments'])
+            elif v['type']=='LL':
+                widget=my_label.My_Label_Layout(v['pos_hint'],v['size_hint'],v['arguments'])
+            elif v['type']=='TI':
+                widget=my_text_input.My_Text_Input(v['pos_hint'],v['size_hint'],v['arguments'])
+            parent_object.add_widget(widget)
+            self.heirarchy[k]=widget
 
 class Add_Button(RelativeLayout):
     def __init__(self,**kwargs):
@@ -109,7 +147,8 @@ class Window_Manager(ScreenManager):
         self.app_screen=App_Screen(name="app_screen")
         self.add_widget(self.layout_screen)
         self.add_widget(self.app_screen)
-        self.current="layout_screen"
+        self.layout_screen.ids._drawing_area.initialize_layout()
+        self.current="app_screen"
 class MyDesignerApp(App):
     def build(self):
         manager=Window_Manager()

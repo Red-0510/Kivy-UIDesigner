244327bc28792f518e1ed822518e0bcf84580c17
Red-0510 <69681031+Red-0510@users.noreply.github.com>
2022-04-21 18:20:01 +0530

    Delete elements directory

--- elements/my_button.py
+++
@@ -1,6 +0,0 @@
-from kivy.uix.button import Button
-
-class My_Button_Layout(Button):
-    def __init__(self,poshints,size,keys,**kwargs):
-        self.heirarchy={}
-        super(My_Button_Layout,self).__init__(pos_hint=poshints,size_hint=size,**keys,**kwargs)
